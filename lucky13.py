4b58faf419f1d082d86ce6d8e2c4593ff1224f84
daxblog <48895834+daxblog@users.noreply.github.com>
2025-01-30 13:26:00 +0100

    Update lucky13.py

--- lucky13.py
+++ lucky13.py
@@ -157,9 +157,10 @@ if __name__ == "__main__":
     start()
     port = int(os.environ.get("PORT", 5000))
     import eventlet
-    eventlet.monkey_patch()  # Zorgt dat eventlet goed werkt met Flask
+eventlet.monkey_patch()  # Nodig voor compatibiliteit met Flask en SocketIO
 
-    socketio.run(app, host="0.0.0.0", port=port, debug=DEBUG_MODE, allow_unsafe_werkzeug=True)
+socketio.run(app, host="0.0.0.0", port=port, debug=DEBUG_MODE, server='eventlet')
+
 
     print("Bot is gestopt.")
     sys.exit(0)

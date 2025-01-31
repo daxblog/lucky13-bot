42ffbb6cbb7e84ec47036a9fe537835504548d5e
daxblog <48895834+daxblog@users.noreply.github.com>
2025-01-31 23:02:40 +0100

    Update lucky13.py

--- lucky13.py
+++ lucky13.py
@@ -15,7 +15,7 @@ import eventlet
 import eventlet.wsgi
 
 # Slack Webhook URL
-SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
+SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
 
 # Functie om Slack notificaties te verzenden
 def send_slack_notification(message):
